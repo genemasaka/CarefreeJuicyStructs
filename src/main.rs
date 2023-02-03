ae32074320413b3721c28adf48fc4817ae31bbde
Eugene Masaka <eugenemasaka777@gmail.com>
2023-02-03 15:22:41 +0300

    methods

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@ struct Album {
     name: String,
     artist: String,
     genre: String,
-    release_year: i32,
+    year: usize,
     price: f64,
+    sales: usize,
+}
+
+impl Album {
+    fn new(name: String, artist: String, genre: String, year: usize, price: f64, sales: usize) -> Album{
+        Self {name, artist, genre, year, price, sales}   
+    }
+
+    fn is_platinum(&self) -> bool {
+        if self.sales >= 1000000 {
+            true
+        } else {
+            false
+        }
+    }
 }
 
 fn main() {
-    let name = String::from("Mr. Morale And The Big Steppers");
-    let artist = String::from("Kendrick Lamar");
-    let genre = String::from("Rap/Hiphop");
-    let release_year = 2022;
-    let price = 10.55;
-    println!("{:#?}", create_album(name, artist, genre, release_year, price));
-}
 
-fn create_album(album_name: String, album_artist: String, album_genre: String, album_release_year: i32, album_price: f64) -> Album {
-    let new_album = Album {
-        name: album_name,
-        artist: album_artist,
-        genre: album_genre,
-        release_year: album_release_year,
-        price: album_price,
-    };
-    new_album
+    let album = Album::new(
+        String::from("Mr. Morale and The Big Steppers"),
+        String::from("Kendrick Lamar"),
+        String::from("HipHop/Rap"),
+        2022,
+        10.55,
+        1039445,
+        );
+    println!("{} is certified platinum: {}", album.name, album.is_platinum());
 }
